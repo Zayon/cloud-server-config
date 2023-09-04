0ae7979564106543cad00987dd1891c4f90e69bd
Godinez Pablo <pablo.godinez@zayon.eu>
2023-09-04 23:13:39 +0200

    Improve cm completion

--- bin/completions/cm-completion.bash
+++ bin/completions/cm-completion.bash
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+fetch_available_services() {
+    local server_config_root service_name service_path services
+    server_config_root=$(realpath $(dirname $(dirname $(dirname "$0"))))
+
+    services=()
+    find "$server_config_root/docker-configs" -type f -name "*.yaml" | while read -r service_path; do
+        // strip the .yaml extension
+        service_name="${service_path%.yaml}"
+        // strip the path prefix
+        service_name="${service_name##*/}"
+        services+=("$service_name")
+    done
+
+    echo "${services[@]}"
+}
+
 _cm_completions()
 {
-    local cur_word prev_word type_list
+    local cur_word prev_word type_list service_list
 
     # COMP_WORDS is an array of words in the current command line.
     # COMP_CWORD is the index of the current word (the one the cursor is
@@ -14,14 +30,16 @@ _cm_completions()
 
     # Commands for cm
     COMPREPLY=() # Array variable storing the possible completions.
-    case "${prev_word}" in
-        cm)
-            type_list="up logs"
-            ;;
-        *)
-            type_list=""
-            ;;
-    esac
+    if [[ ${#COMP_WORDS[@]} -eq 2 ]]; then
+        # Completing the command after "cm"
+        type_list="up enable disable logs"
+        COMPREPLY=( $(compgen -W "${type_list}" -- ${cur_word}) )
+    elif [[ ${#COMP_WORDS[@]} -ge 3 ]]; then
+        # Completing the service after "cm up" or similar command
+        service_list=$(fetch_available_services)
+
+        COMPREPLY=( $(compgen -W "${service_list}" -- ${cur_word}) )
+    fi
 
     # Generate possible matches and store them in the array variable COMPREPLY
     COMPREPLY=( $(compgen -W "${type_list}" -- ${cur_word}) )
@@ -29,3 +47,4 @@ _cm_completions()
 
 # Register our completion function.
 complete -F _cm_completions cm
+
