1390420e2cce0b9f34b23c665b6e6f091647671c
Godinez Pablo <pablo.godinez@zayon.eu>
2023-09-04 23:16:44 +0200

    Improve cm completion again

--- bin/completions/cm-completion.bash
+++ bin/completions/cm-completion.bash
@@ -1,48 +1,57 @@
 #!/bin/bash
 
-fetch_available_services() {
-    local server_config_root service_name service_path services
-    server_config_root=$(realpath $(dirname $(dirname $(dirname "$0"))))
+# Exit on error, uninitialized variable, or error in a pipeline.
+set -euo pipefail
 
-    services=()
-    find "$server_config_root/docker-configs" -type f -name "*.yaml" | while read -r service_path; do
-        // strip the .yaml extension
+fetch_available_services() {
+    local service_name service_path
+    local -a services # Declare services as an indexed array
+
+    if [[ -z "${SERVER_CONFIG_ROOT:-}" ]]; then
+        echo "SERVER_CONFIG_ROOT is not set"
+        return 1
+    fi
+
+    while read -r service_path; do
+        # strip the .yaml extension
         service_name="${service_path%.yaml}"
-        // strip the path prefix
+        # strip the path prefix
         service_name="${service_name##*/}"
         services+=("$service_name")
-    done
+    done < <(find "$SERVER_CONFIG_ROOT/docker-configs" -type f -name "*.yaml")
 
     echo "${services[@]}"
 }
+
 
 _cm_completions()
 {
     local cur_word prev_word type_list service_list
 
+    # Initialize COMPREPLY array
+    COMPREPLY=()
+
     # COMP_WORDS is an array of words in the current command line.
-    # COMP_CWORD is the index of the current word (the one the cursor is
-    # in). So COMP_WORDS[COMP_CWORD] is the current word.
     cur_word="${COMP_WORDS[COMP_CWORD]}"
 
     # Previous word on the command line.
     prev_word="${COMP_WORDS[COMP_CWORD-1]}"
 
     # Commands for cm
-    COMPREPLY=() # Array variable storing the possible completions.
     if [[ ${#COMP_WORDS[@]} -eq 2 ]]; then
         # Completing the command after "cm"
         type_list="up enable disable logs"
         COMPREPLY=( $(compgen -W "${type_list}" -- ${cur_word}) )
     elif [[ ${#COMP_WORDS[@]} -ge 3 ]]; then
-        # Completing the service after "cm up" or similar command
-        service_list=$(fetch_available_services)
+        # If SERVER_CONFIG_ROOT is not set, return without doing anything.
+        if [[ -z "${SERVER_CONFIG_ROOT:-}" ]]; then
+            return 0
+        fi
 
+        service_list=$(fetch_available_services)
+        # Completing the service after "cm up" or similar command
         COMPREPLY=( $(compgen -W "${service_list}" -- ${cur_word}) )
     fi
-
-    # Generate possible matches and store them in the array variable COMPREPLY
-    COMPREPLY=( $(compgen -W "${type_list}" -- ${cur_word}) )
 }
 
 # Register our completion function.
